a236c8d41237ba7470f72c0e5a37c42eb8decb56
Chris Kozma <ckozma@live.com>
2021-11-23 11:27:50 -0500

    comments!

--- scrapePHRF.py
+++ scrapePHRF.py
@@ -2,7 +2,11 @@ import pandas as pd
 
 url = 'http://www.phrfne.org/page/handicapping/base_handicaps'
 dfs = pd.read_html(url)
+#grab the third table on the page
 table = dfs[2]
+#get only the columns we want
 columns = table[[0,2]]
+#delete row 0, not part of the data
 columns = columns.drop(0)
+
 print(columns)
