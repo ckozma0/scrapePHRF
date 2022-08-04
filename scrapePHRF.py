9c32d5370095b5da790c688e8ad1a82747f5657c
Chris Kozma <ckozma@live.com>
2022-08-03 20:45:57 -0400

    typo

--- scrapePHRF.py
+++ scrapePHRF.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 url = 'http://www.phrfne.org/page/handicapping/base_handicaps'
 dfs = pd.read_html(url)
-#grab the third table on the page
+#grab the first table on the page
 table = dfs[1]
 #get only the columns we want
 table = table[[0,2]]
